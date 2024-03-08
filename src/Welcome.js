c5ac1e2a64155dcf78f5b8154b422a7f0b372fbb
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-03-08 14:16:30 -0500

    making welcome

---
+++ src/Welcome.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Welcome = () => {
+  return(
+    <div>
+      <h1>Nick.Blog</h1>
+      <div>
+        <ul>
+          <li>Recent Blogs</li>
+          <li>Code Related</li>
+          <li>Family/Friends</li>
+          <li>Work</li>
+          <li>Other Topics</li>
+          <li>Support Nick.Blog</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Welcome
