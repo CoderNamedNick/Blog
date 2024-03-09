2c29bd2678a068a9e000dad1bbcd833c5420c20a
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-03-09 14:49:47 -0500

    finished header making intro

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,18 +2,23 @@ import React from "react";
 
 const Welcome = () => {
   return(
-    <div className="Main-welcome-div">
-      <h1>Nick.Blog</h1>
-      <div className="list-div">
-        <ul className="welcome-list">
-          <li>Recent Blogs</li>
-          <li>Code Related</li>
-          <li>Family/Friends</li>
-          <li>Work</li>
-          <li>Other Topics</li>
-        </ul>
+    <div>
+      <div className="Header-welcome-div">
+        <h1>Nick.Blog</h1>
+        <div className="list-div">
+          <ul className="welcome-list">
+            <li>Recent Blogs</li>
+            <li>Code Related</li>
+            <li>Family/Friends</li>
+            <li>Work</li>
+            <li>Other Topics</li>
+          </ul>
+        </div>
+        <h3>Support Nick.Blog</h3>
       </div>
-      <h3>Support Nick.Blog</h3>
+      <div className="intro-div">
+        <h1>Who Am I</h1>
+      </div>
     </div>
   )
 }
