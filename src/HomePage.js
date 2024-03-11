9851a3b8176c5ae669cd57e47841e8038b662abf
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-03-11 17:56:09 -0400

    making homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,7 +3,33 @@ import React from "react";
 const HomePage = () => {
   return(
     <div>
-      hello
+      <h1>Code Related</h1>
+      <div>
+        <h3>See Coding Blogs</h3>
+      </div>
+      <div className="Coding-Blogs-Grid-Container">
+        <div className="Grid-Items">
+          Topic: TBR
+          <br></br>
+          Date: TBR
+          <br></br>
+          Thoughts: TBR
+        </div>
+        <div className="Grid-Items">
+          Topic: TBR
+          <br></br>
+          Date: TBR
+          <br></br>
+          Thoughts: TBR
+        </div>
+        <div className="Grid-Items">
+          Topic: TBR
+          <br></br>
+          Date: TBR
+          <br></br>
+          Thoughts: TBR
+        </div>
+      </div>
     </div>
   )
 }
