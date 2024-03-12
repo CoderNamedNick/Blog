cc1768679e898d453343c7cc1d9b96b40f9f40f5
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-03-12 12:09:11 -0400

    making blog Template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import Welcome from './Welcome';
 import HomePage from './HomePage';
 
 function App() {
+  const [OnBlog, setOnBlog] = useState(false)
+
+
+  const HandleBlogClick = () => {
+    setOnBlog(true)
+  }
+
   return (
     <div className="Main-App-Div">
-      <Welcome />
-      <HomePage />
+      {!OnBlog && (
+        <div>
+        <Welcome BlogClick={HandleBlogClick} />
+        <HomePage BlogClick={HandleBlogClick}/>
+        </div>
+      )}
+      {OnBlog && (
+        <div className="Blog-Div">
+
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/BlogTemplate.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Welcome from "./Welcome";
+
+const BlogTemplate = () => {
+  return(
+    <div>
+      <Welcome />
+      <br></br>
+      <br></br>
+      <br></br>
+      <div>
+        Put Blog Here
+      </div>
+    </div>
+  )
+}
+
+export default BlogTemplate
