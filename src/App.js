aa69e9e0ddc383beed03809d42e2449e8e0522b6
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-03-13 14:46:03 -0400

    made route work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import HomePage from './HomePage';
 
 function App() {
-
   return (
-    <div className="Main-App-Div">
-        <Header/>
-      <div className='Content-div'>
-        <HomePage />
+    <Router>
+      <div className="Main-App-Div">
+        <Header />
+        <div className='Content-div'>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
