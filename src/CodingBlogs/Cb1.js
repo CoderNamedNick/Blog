883d4814677a3617c62d22c0543bda635ef8c1c8
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-03-15 14:24:39 -0400

    made first blog eligable

--- src/CodingBlogs/Cb1.js
+++ src/CodingBlogs/Cb1.js
@@ -20,17 +20,30 @@ const Cb1 = () => {
       <br></br>
       <br></br>
       <p className="All-coding-p">
-        Hello and welcome to the first Offical Blog post of Nick.Blog! i think there is no better place than to start with how im making this blog.
-        I am making this blog first and foremost for my portfolio and so that not only you but employers can get to know me to.
-        This blog is made from the React Libary, with a few NPM add ons but mostly just React. doing this with react is alot easier then what just 
-        plane JS code can be. This blog has yet to give me massive trouble but I did learn some New things. I learned Links and Routes, ect.
-        If you dont know what that is It how react Apps have "Multiple Pages". Doing this blog makes me feel calm as im not rushing to get anything new out as where my other
-        projects i felt rushed and stress to do something "cool" or "Advanced" with most of this Its Just simple JSX.
+        Hello and welcome to the first Official Blog post of Nick.Blog!<br></br>
+        I think there is no better place than to start than with how I'm making this blog. I am creating this blog, first and foremost, for my portfolio, so not only can you get to know me, but employers can too.
+        This blog is built using the React Library, with a few NPM add-ons, but mostly just React.
+        Doing this with React is a lot easier than what plain JS code can be.<br></br>
+        This blog has yet to give me massive trouble, but I did learn some new things. I learned about Links, Routes, etc. If you don't know what that is, it's how React Apps have "Multiple Pages". 
+        Doing this blog makes me feel calm, as I'm not rushing to get anything new out, whereas with my other projects, I felt rushed and stressed to do something "cool" or "advanced". With most of this, it's just simple JSX.
       </p>
       <p className="All-coding-p">
-        From me to you ill pass a few tips to make a blog if in the future you see yourself doing the same.
-        Step 1: Do It. Me myself im a huge procrastinater instead of reveiwing code or look up react courses in 2 hours ill probably be nose deep in a video game
-        so hey save the trouble of not doing it and do it.
+        From me to you, I'll share a few tips for making a blog, in case you find yourself doing the same in the future.
+        <br></br>
+        <br></br>
+        Step 1: Just do it. Personally, I'm a huge procrastinator. Instead of reviewing code or looking up React courses, in two hours, I'd probably be nose-deep in a video game. So, save yourself the trouble of not doing it and just do it.
+        <br></br>
+        <br></br>
+        Step 2: Choose a framework, library, or just plain JS. I'm a huge advocate of plain JS. I believe you can accomplish anything you can do with a framework. "But Nick, it will take so much longer!" Yes, but you learn the fundamentals, where everything started: just HTML, CSS, and JS.
+        <br></br>
+        <br></br>
+        Step 3: Make it your website. I'm known as a boring guy, and my blog might reflect that. But you need to show that it's yours and not some marketing tool.
+        <br></br>
+        <br></br>
+        Final Step: Learn. Improve. Accomplish something. You're never going to improve your coding skills by just doing stuff you've already done. Like a whole month ago, I didn't even know how to do anything in React. Now, look at this site. Your main goal should be to learn.
+      </p>
+      <p className="All-coding-p">
+        Thank You For Reading
       </p>
     </div>
   );
