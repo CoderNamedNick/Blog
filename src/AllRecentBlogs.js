d1f936749258f83beb48e9c86f97b238124bc586
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-03-16 13:13:47 -0400

    made all recent blogs tab

--- src/AllRecentBlogs.js
+++ src/AllRecentBlogs.js
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 const ALLRecentBlogs = () => {
   return(
-    <div className="All-coding-Main-div">
+    <div className="All-Recent-Main-div">
       <br></br>
       <br></br>
       <br></br>
       <br></br>
       <br></br>
-      <h1>ALL CODING BLOGS</h1>
+      <h1>MOST RECENT BLOGS</h1>
       <h2>
-        Most Recent Blogs
+        Recent Blogs about any topics
       </h2>
-      <div className="Coding-Blogs-Grid-Container">
+      <div className="Recent-blogs-Grid-Container">
         <Link to="/AllCoding/CB1">
-        <div className="Coding-Grid-Items">
+        <div className="Recent-Grid-Items">
           Topic: Making The Blog
           <br></br>
           Date:  3/15/24
@@ -23,21 +23,21 @@ const ALLRecentBlogs = () => {
           Thoughts: Relaxing, Easy, New
         </div>
         </Link>
-        <div className="Coding-Grid-Items">
+        <div className="Recent-Grid-Items">
           Topic: NOT AVAILABLE
           <br></br>
           Date: NOT AVAILABLE
           <br></br>
           Thoughts: NOT AVAILABLE
         </div>
-        <div className="Coding-Grid-Items">
+        <div className="Recent-Grid-Items">
           Topic: NOT AVAILABLE
           <br></br>
           Date: NOT AVAILABLE
           <br></br>
           Thoughts: NOT AVAILABLE
         </div>
-        <div className="Coding-Grid-Items">
+        <div className="Recent-Grid-Items">
           Topic: NOT AVAILABLE
           <br></br>
           Date: NOT AVAILABLE
