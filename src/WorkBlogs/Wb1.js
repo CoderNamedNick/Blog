6e660ef4b1c74b60e512adc3ace4e1cc4b7cb3cb
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-03-18 11:56:16 -0400

    made Wb1

--- src/WorkBlogs/Wb1.js
+++ src/WorkBlogs/Wb1.js
@@ -20,27 +20,30 @@ const Wb1 = () => {
       <br></br>
       <br></br>
       <p className="All-Work-p">
-        Hello and welcome to the first Official Blog post of Nick.Blog!<br></br>
-        I think there is no better place than to start than with how I'm making this blog. I am creating this blog, first and foremost, for my portfolio, so not only can you get to know me, but employers can too.
-        This blog is built using the React Library, with a few NPM add-ons, but mostly just React.
-        Doing this with React is a lot easier than what plain JS code can be.<br></br>
-        This blog has yet to give me massive trouble, but I did learn some new things. I learned about Links, Routes, etc. If you don't know what that is, it's how React Apps have "Multiple Pages". 
-        Doing this blog makes me feel calm, as I'm not rushing to get anything new out, whereas with my other projects, I felt rushed and stressed to do something "cool" or "advanced". With most of this, it's just simple JSX.
+        Of course, here's your text with corrections:
+        Today is 3/18/24, and it's the very first day of "Work," even though I don't have a job.
+         You can still work, and I mean work towards a job in my case. Yesterday I did some learning, and I want my workplace blogs to not only help myself jog my memory and write it down but also to assist anyone in my shoes going through what I am.
+        Another quick note is before you learn or do anything new, or maybe you have 20+ years, just remember that you aren't expected to master anything in one day, and it will never happen. So, take your time and learn/work at your pace, and that's improvement.
+        Yesterday I watched some videos on APIs and Promises, and I wanna talk first on APIs.
       </p>
       <p className="All-Work-p">
-        From me to you, I'll share a few tips for making a blog, in case you find yourself doing the same in the future.
+        WHY ARE APIs SO CONFUSING TO LEARN! Trust me, I've seen countless videos trying to explain what an API is.
+        API stands for Application Programming Interface..... which tells you nothing about what it is. In short terms, it is "a set of rules and protocols that allows different software applications to communicate with each other."
+        And in Nick Terms, it's how computers talk with other computers. I like to think that it's like a firm handshake.
+        When you walk up to your boss, you shake his hand. Computers shake hands and say, "Here is what I can do and what I have. What do you want?"
+        Trust me, if you're struggling with this idea, we all are or were! And if you didn't, lucky you!
+      </p>
+      <p className="All-Work-p">
+        Promises. This is actually super simple. I love it when coding terms actually make sense for what the name implies.
+        Promises are exactly what you think they are; they are a promise that you get when you ask another computer out there something. When you ask a computer something, for example, 
+        you ask a database what your username is, the computer sends you a promise. "I PROMISE I will try to get that for you." It then goes and returns what it got. 
+        It normally comes back with a code saying "Got it" or a code error saying "Sorry, I dropped it." With promises, you can use a '.then' after the promise to do something after you get a response.
+        I find that normally you just put a ".then" to make it into JSON. JSON is another topic for another day.
+      </p>
+      <p className="All-Work-p">
+        I went on a bit of a coding ramble, but I still want to leave this in the work section because work comes with struggles and times when you don't understand anything you are doing.
         <br></br>
-        <br></br>
-        Step 1: Just do it. Personally, I'm a huge procrastinator. Instead of reviewing code or looking up React courses, in two hours, I'd probably be nose-deep in a video game. So, save yourself the trouble of not doing it and just do it.
-        <br></br>
-        <br></br>
-        Step 2: Choose a framework, library, or just plain JS. I'm a huge advocate of plain JS. I believe you can accomplish anything you can do with a framework. "But Nick, it will take so much longer!" Yes, but you learn the fundamentals, where everything started: just HTML, CSS, and JS.
-        <br></br>
-        <br></br>
-        Step 3: Make it your website. I'm known as a boring guy, and my blog might reflect that. But you need to show that it's yours and not some marketing tool.
-        <br></br>
-        <br></br>
-        Final Step: Learn. Improve. Accomplish something. You're never going to improve your coding skills by just doing stuff you've already done. Like a whole month ago, I didn't even know how to do anything in React. Now, look at this site. Your main goal should be to learn.
+        WORK HARD. And please, never give up.
       </p>
       <p className="All-Work-p">
         Thank You For Reading
