b60c4ae9f4510542ea8e46a0cdf33b3b2c26b097
CoderNamedNick <153879485+CoderNamedNick@users.noreply.github.com>
2024-03-19 13:06:40 -0400

    made support page

--- src/SupportPage.js
+++ src/SupportPage.js
@@ -10,10 +10,34 @@ const SupportPage = () => {
       <br></br>
       <br></br>
       <div>
-        <h2>How To Suport Nick.Blogs</h2>
-
-
+        <h1>How To Suport Nick.Blogs</h1>
+        <br></br>
+        <br></br>
+        <br></br>
+        <div>
+          <p className="Support-p">
+            <a href="mailto:nclloyd422@hotmail.com" >Email Me Ideas</a>: This helps me come up with new ideas or even to help you on topics
+            you might what to know more about. Also Emailing me would give be motivation to Blogs or and put my Ideas!
+          </p>
+        </div>
+        <div>
+          <p className="Support-p">
+            Go to <a href="https://github.com/CoderNamedNick">GitHub</a> and Star the repository.
+            I made this blog form Scratch and want people to see its Glory!
+          </p>
+        </div>
+        <div>
+          <p className="Support-p">
+            Look at my <a href="https://codernamednick.github.io/react-portfolio/">Portfolio</a> If you are Someone in need of a website. I would love 
+            to try to work for you. If im not to busy "most days im not".
+          </p>
+        </div>
       </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
 
     </div>
   )
